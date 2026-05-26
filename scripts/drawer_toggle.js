5f2ccabff36807190ecead20cb604c80abc8349e
SergeyKotenkov <kotenkov272006@gmail.com>
2026-05-26 16:03:49 +0200

    .

--- scripts/drawer_toggle.js
+++ scripts/drawer_toggle.js
@@ -2,6 +2,7 @@ export function drawer_toggle() {
 
 const drawer_button = document.getElementById("drawer_button");
 const overlay = document.getElementById("overlay");
+const drawer_close_button = document.getElementById("drawer_close_button");
 
 function openDrawer() {
     document.getElementById("drawer").classList.add("open");
@@ -13,9 +14,24 @@ function closeDrawer() {
     document.getElementById("overlay").classList.remove("show");
 }
 
+function activeMenuSet(x) {
+    localStorage.setItem("activeMenu", x);
+}
+
+const drawer = document.getElementById("drawer");
+const menus = drawer.querySelectorAll("a");
+const headerActiveMenuText = document.getElementById("activeMenuText");
+
+menus.forEach(element => {
+    const attribute = element.getAttribute("data-menu");
+    if(attribute == localStorage.getItem("activeMenu")) {element.classList.add("active"); headerActiveMenuText.innerText = attribute;}
+    else if (localStorage.getItem("activeMenu") == null && attribute == "home") element.classList.add("active"); 
+    else element.classList.remove("active");
+    element.addEventListener("click", () => activeMenuSet(attribute));
+});
 
 drawer_button.addEventListener("click", openDrawer);
 overlay.addEventListener("click", closeDrawer);
-
+drawer_close_button.addEventListener("click", closeDrawer);
 
 }
