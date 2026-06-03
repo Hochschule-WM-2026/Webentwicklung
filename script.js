1ef5db364291fa6a3d79718781c12b550ca7482c
styvedontsop-png <styvedontsop@icloud.com>
2026-06-03 10:41:46 +0200

    eke

---
+++ script.js
@@ -0,0 +1,12 @@
+//document.querySelector("body").style.background= "linear-gradient(to right,  #4b0082, #8b00ff)";    
+//document.querySelector(".quiz_button").style.background = "red";
+const button1 = document.querySelector(".quiz_button");
+  console.log(button1);
+
+  button1.addEventListener('click',() =>{
+    {
+    button1.style.background = "green";  
+    button1.style.height = "100px";
+    } 
+  })
+ 
